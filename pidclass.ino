5841b131a6d981e7a93df5ff2e83998cb8d520d4
yashpermalla <67378117+yashpermalla@users.noreply.github.com>
2025-04-19 19:03:37 -0400

    pid class if we want it

---
+++ pidclass.ino
@@ -0,0 +1,67 @@
+typedef void (*ActFunc) (float ctrl);
+typedef void (*MeasFunc) ();
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
+
+
+class PID {
+  public:
+    float kp;
+    float ki;
+    float kd;
+
+    float preverror;
+    long prevtime;
+    float integral;
+
+    float target;
+    ActFunc act;
+    MeasFunc meas;
+
+    PID(float kp_, float ki_, float kd_) : kp(kp_), ki(ki_), kd(kd_){}
+
+    void setControl(ActFunc func) {
+      act = func;
+    }
+
+    void setMeasurement(MeasFunc func) {
+      meas = func;
+    }
+
+    void initialize() {
+      preverror = meas() - target;
+      prevtime = millis();
+      integral = 0;
+    }
+
+    float PIDstep() {
+      long newtime = millis();
+      long dt = newtime - prevtime;
+      
+      float error = meas() - target;
+      float derivative = (error - preverror) / dt;
+      integral += error * dt;
+
+      float ctrlsignal = kp * error + ki * integral + kd * derivative;
+      act(ctrlsignal);
+
+      prevtime = newtime;
+      preverror = error;
+
+      return abs(error);
+    }
+
+    void moveTo(float targ, float eps) {
+      target = targ;
+      initialize();
+      while (PIDstep() > eps) {}
+    }
+};
